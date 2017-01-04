82d351fcd016680117e42be519f5b89729dd4f15
Lrakulka <olecsandr32@gmail.com>
2017-01-05 01:38:01 +0200

    Track page updated. Field freePlaces removed from equals and hash

--- src/main/java/ejb/entity/Track.java
+++ src/main/java/ejb/entity/Track.java
@@ -80,9 +80,7 @@ public class Track extends AbstractDBObject {
         if (startLocation != null ? !startLocation.equals(track.startLocation) : track.startLocation != null)
             return false;
         if (stopLocation != null ? !stopLocation.equals(track.stopLocation) : track.stopLocation != null) return false;
-        if (maxCompanions != null ? !maxCompanions.equals(track.maxCompanions) : track.maxCompanions != null)
-            return false;
-        return freePlaces != null ? freePlaces.equals(track.freePlaces) : track.freePlaces == null;
+        return maxCompanions != null ? maxCompanions.equals(track.maxCompanions) : track.maxCompanions == null;
     }
 
     @Override
@@ -91,7 +89,6 @@ public class Track extends AbstractDBObject {
         result = 31 * result + (startLocation != null ? startLocation.hashCode() : 0);
         result = 31 * result + (stopLocation != null ? stopLocation.hashCode() : 0);
         result = 31 * result + (maxCompanions != null ? maxCompanions.hashCode() : 0);
-        result = 31 * result + (freePlaces != null ? freePlaces.hashCode() : 0);
         return result;
     }
 }
